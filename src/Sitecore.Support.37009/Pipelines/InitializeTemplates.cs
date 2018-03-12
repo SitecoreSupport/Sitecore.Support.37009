42cdc42b7c6aa4e0423d7172ab983ea0dbd3189b
DK\msb <msb@sitecore.net>
2018-03-12 16:58:20 -0400

    Initialize method is executed for CatalogDataProvider

---
+++ src/Sitecore.Support.37009/Pipelines/InitializeTemplates.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Commerce.Connect.CommerceServer.Catalog;
+using Sitecore.Configuration;
+using Sitecore.Data.DataProviders;
+using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Configuration.Provider;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Commerce.Connect.CommerceServer.Pipelines
+{
+    public class InitializeTemplates
+    {
+        public void Process(PipelineArgs args)
+        {
+            var database = Factory.GetDatabase("master");
+            var dataProviders = database.GetDataProviders();
+
+            foreach (var provider in dataProviders)
+            {
+                if (provider is CatalogDataProvider)
+                {
+                    typeof(CatalogDataProvider).GetMethod("InitializeTemplates", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Invoke(provider, new object[] { provider.Database });
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Sitecore.Support.37009/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.37009")]
+[assembly: AssemblyProduct("Sitecore.Support.37009")]
+[assembly: ComVisible(false)]
